b015f431547fabbd69448d0260de9d70a5627ef9
dilaraseren <dilara153232@gmail.com>
2023-08-21 12:05:15 +0300

    first commit

---
+++ models.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+
+from sqlalchemy import Column, Integer, String, DateTime, create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+Base = declarative_base()
+
+
+class UserModel(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    birth = Column(DateTime)
+    create_on = Column(DateTime, default=datetime.utcnow)
+
+    @property
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def __repr__(self):
+        return (
+            f'UserModel(id={self.id}, first_name={self.first_name},'
+            f'last_name={self.last_name}, birth={self.birth},'
+            f'create_on={self.create_on})'
+        )
+
+
+users = [
+    UserModel(first_name='Bob', last_name='Preston', birth=datetime(1980, 5, 2)),
+    UserModel(first_name='Susan', last_name='Sage', birth=datetime(1979, 6, 12)),
+]
+
+session_maker = sessionmaker(bind=create_engine('sqlite:///models.db'))
+
+
+def create_users():
+    with session_maker() as session:
+        for user in users:
+            session.add(user)
+        session.commit()
+
+
+with session_maker() as session:
+    user_records = session.query(UserModel).all()
+    for user in user_records:
+        print(user.full_name)
